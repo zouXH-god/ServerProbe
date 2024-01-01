37b0b0b096a135ad5e992a9956dc04922c1efb88
zouXH-god <77649130+zouXH-god@users.noreply.github.com>
2024-01-01 20:07:51 +0800

    新增，服务器掉线提醒

--- 服务端/server.py
+++ 服务端/server.py
@@ -1,10 +1,11 @@
 import json
+import time
 
 import flask
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from tools import format_time
+from tools import format_time, config, send_email
 
 app = flask.Flask(__name__)
 with open("server_list.json", "r", encoding="utf-8") as f:
@@ -12,7 +13,7 @@ with open("server_list.json", "r", encoding="utf-8") as f:
 # 定时请求
 scheduler = BackgroundScheduler()
 
-
+# 定义定时任务
 def timed_task():
     for server in server_list:
         url = f"http://{server['server_ip']}:{server['server_port']}/get_system_info"
@@ -23,13 +24,21 @@ def timed_task():
             # 存储数据
             server["server_info"] = response
             server["server_static"] = "已连接"
+            server["update_time"] = time.time()
         except:
             server["server_info"] = None
             server["server_static"] = "未连接"
+            # 判断是否超过设定时间，超过则发送邮件
+            update_time = server.get("update_time", 0)
+            if time.time() - update_time > 60 * config["setting"]["lostTime"]:
+                # 发送邮件
+                send_email(config["setting"]["toEmail"], "【探针提醒】服务器掉线", f"服务器<{server['server_name']}>掉线，请及时检查！")
+                server["update_time"] = time.time()
 
 # 添加定时任务，间隔5秒
 scheduler.add_job(timed_task, 'interval', seconds=5)
 scheduler.start()
+timed_task()
 
 
 @app.route('/')
--- 服务端/tools.py
+++ 服务端/tools.py
@@ -1,3 +1,18 @@
+import json
+import os
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(root_path, "setting.json")
+with open(config_path, "r", encoding="utf-8") as fp:
+    config = json.load(fp)
+
+
 def format_time(minutes):
     # 定义时间单位
     minutes_per_hour = 60
@@ -16,3 +31,42 @@ def format_time(minutes):
     else:
         return f"{remaining_minutes}分"
 
+
+# 发送邮件
+def send_email(to_addr, subject, content):
+    """
+    发送电子邮件。
+
+    :param to_addr: 收件人地址
+    :param subject: 邮件主题
+    :param content: 邮件内容
+    :return: None
+    """
+
+    # 第三方 SMTP 服务
+    mail_host = config["email"]["smtp"]  # 设置服务器
+    mail_port = config["email"]["port"]  # 设置服务器端口号
+    mail_user = config["email"]["email"]  # 用户名
+    mail_pass = config["email"]["password"]  # 口令
+    receivers = [to_addr]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    # 创建一个 MIMEText 对象，指定邮件正文和 MIME 类型
+    message = MIMEMultipart()
+    message['From'] = mail_user
+    message['To'] = to_addr
+    message['Subject'] = subject
+
+    html = MIMEText(content, 'html', 'utf-8')
+    message.attach(html)
+    try:
+        # 使用 SSL 创建 SMTP 对象
+        smtp_obj = smtplib.SMTP_SSL(mail_host, mail_port)
+        smtp_obj.login(mail_user, mail_pass)  # 登录
+        smtp_obj.sendmail(mail_user, receivers, message.as_string())  # 发送邮件
+        smtp_obj.quit()  # 关闭连接
+    except smtplib.SMTPException as e:
+        print("Error: 无法发送邮件", e)
+
+
+if __name__ == '__main__':
+    send_email('2488252513@qq.com', "subject", "content")
